9f979930bc2685881f7f18182ab9faaaf82a7648
develop-ddl <997004866@qq.com>
2018-06-06 23:47:50 +0800

    差不多完事儿了，不写了。。。

--- myCrawler/spiders/dianpingSpider.py
+++ myCrawler/spiders/dianpingSpider.py
@@ -4,6 +4,7 @@ from scrapy.spiders import CrawlSpider
 from scrapy.selector import Selector
 from myCrawler.utils.mongodb_utils import *
 import datetime
+from lxml import etree
 
 # AJAX网页特点
 # 1. 页面加载快速
@@ -39,7 +40,8 @@ class Dianping(CrawlSpider):
         '胖哥俩肉蟹煲'
     ]
     start_urls = [
-        'http://210.46.97.225/gov_corpus/corpus_look1.html'
+        # 'http://210.46.97.225/gov_corpus/corpus_look1.html'
+        'http://xiaodu.baidu.com/saiya/gaokao/schooldetail?schoolId=8&tab=2&logFrom=sl_8%27'
         # "https://www.dianping.com/search/keyword/3/0_"
         # 'http://www.dianping.com/shop/97567356'
         # 'http://www.dianping.com/shop/97567356/review_all/p2'
@@ -50,13 +52,19 @@ class Dianping(CrawlSpider):
     #         yield SplashRequest(url, self.parse, args={'wait': 0.5})
 
     def parse(self, response):
-        # res_url = response.url
+        res_url = response.url
         # with open('../crawler_files/dianping_shop'+nowTime+'.html', 'w') as f:
         #     f.write(res_url)
         #     f.write('\n\n')
         #     f.write(response.text)
 
-        print(response.text)
+        # print(response.text.encode('utf-8').decode('unicode_escape'))
+
+        # selector = Selector(response=response)
+        # ques_list = selector.xpath('//*[@class ="c-container tie-container"]')
+        # print(ques_list)
+
+
         #
         # selector = Selector(response=response)
         # web_content = selector.xpath('//*[@class="comment-item"]')
